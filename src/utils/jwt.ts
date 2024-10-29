36ae9c0e22188f454ff496a192e5935aba902e33
Daniel Navarro <daniel.navarro@newtech.consulting>
2024-10-29 23:08:53 +0000

    jwt

---
+++ src/utils/jwt.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+import { Types } from 'mongoose'
+
+type UserPayload = {
+    id: Types.ObjectId
+}
+
+export const generateJWT = (payload: UserPayload) => {
+    const data = {
+        id: payload.id
+    }
+    const token = jwt.sign(data, process.env.JWT_SECRET, {
+        expiresIn: '6m'
+    })
+    return token
+}
